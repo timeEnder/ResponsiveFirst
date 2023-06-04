834a1df03b8cc6a915a6aadf810ccfd6641b0769
timeEnder <sh.s.ganiev@gmail.com>
2023-06-04 20:07:59 +0500

    message

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import style from './App.css';
+import Header from "./components/Header/Header";
+
+function App() {
+
+  return (
+    <div className={style.container}>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from './style.module.css';
+
+export default function Header(){
+    return(
+        <div className={style.container}>
+           <div className={style.label}>TaskPro</div>
+            <div className={style.menu}>
+                <div className={style.round}/>
+                <div className={style.round}/>
+                <div className={style.round}/>
+                <div className={style.round}/>
+            </div>
+        </div>
+    )
+}
