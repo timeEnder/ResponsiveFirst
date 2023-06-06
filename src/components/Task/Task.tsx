530107436d2f5650024ca9d26879b2b1e1944159
timeEnder <sh.s.ganiev@gmail.com>
2023-06-06 18:12:13 +0500

    chan

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -4,6 +4,10 @@ export default function Task(){
     return(
         <div className={style.container}>
             <div className={style.title}><div className={'font-bold text-2xl'}>Pending Task</div><div className={'text-[#5fbc7f] font-bold'}>See All</div></div>
+            <div className={style.line}>
+                <div className={style.block1}></div>
+                <div className={style.block2}></div>
+            </div>
         </div>
     )
 }
