82d5854edb1d6b4d22dad110391e79ebe6c403ea
timeEnder <sh.s.ganiev@gmail.com>
2023-06-07 20:23:46 +0500

    chan

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -7,10 +7,8 @@ export default function Task(){
             <div className={style.title}><div className={'font-bold text-2xl'}>Pending Task</div><div className={'text-[#5fbc7f] font-bold'}>See All</div></div>
             <div className={style.line}>
                 <div className={style.block1}>
-                    <div className={style.inline}><img src={phone}/>
-                        <div className={style.but}>47% Complete</div>
-                    </div>
-                    APP DESIGN
+                    <div className={style.inline}><img src={phone}/><div className={style.but}>47% Complete</div></div>
+                    <div className={style.text}>APP DESIGN</div>
                 </div>
                 <div className={style.block2}>WEB</div>
             </div>
