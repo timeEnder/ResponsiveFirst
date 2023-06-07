ad04aa31a69b175d4ccb493fa4374d07252bc652
timeEnder <sh.s.ganiev@gmail.com>
2023-06-07 19:12:21 +0500

    chan

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,12 @@
 import style from './style.module.css';
+import phone from '../../resourses/phone.png';
 
 export default function Task(){
     return(
         <div className={style.container}>
             <div className={style.title}><div className={'font-bold text-2xl'}>Pending Task</div><div className={'text-[#5fbc7f] font-bold'}>See All</div></div>
             <div className={style.line}>
-                <div className={style.block1}>APP DESIGN</div>
+                <div className={style.block1}><img src={phone}/>APP DESIGN</div>
                 <div className={style.block2}>WEB</div>
             </div>
             <div className={style.line}>
