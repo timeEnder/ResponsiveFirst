5b6c82877147fc8bd39a65f3b39349c9b2f72f8d
timeEnder <sh.s.ganiev@gmail.com>
2023-06-08 16:11:01 +0500

    chan

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,5 +1,8 @@
 import style from './style.module.css';
 import phone from '../../resourses/phone.png';
+import laptop from '../../resourses/laptop.png';
+import clients from '../../resourses/clients.png';
+import ball from '../../resourses/ball.png';
 
 export default function Task(){
     return(
@@ -9,12 +12,25 @@ export default function Task(){
                 <div className={style.block1}>
                     <div className={style.inline}><img src={phone}/><div className={style.but}>47% Complete</div></div>
                     <div className={style.text}>APP DESIGN</div>
+                    <div className={style.text2}>App Design Task</div>
                 </div>
-                <div className={style.block2}>WEB</div>
+                <div className={style.block2}>
+                    <div className={style.inline}><img src={laptop}/></div>
+                    <div className={style.text}>WEB</div>
+                    <div className={style.text2}>Web design</div>
+                </div>
             </div>
             <div className={style.line}>
-                <div className={style.block3}>CLIENT</div>
-                <div className={style.block4}>DRIBBBLE</div>
+                <div className={style.block3}>
+                    <div className={style.inline}><img src={clients}/></div>
+                    <div className={style.text}>CLIENT</div>
+                    <div className={style.text2}>Ave studio des...</div>
+                </div>
+                <div className={style.block4}>
+                    <div className={style.inline}><img src={ball}/></div>
+                    <div className={style.text}>DRIBBBLE</div>
+                    <div className={style.text2}>Create dribbble shot</div>
+                </div>
             </div>
         </div>
     )
