bf8d22fa330977710abdc5e0e6fb010a6229a172
timeEnder <sh.s.ganiev@gmail.com>
2023-06-09 11:20:30 +0500

    chan

--- src/components/AppDesignUser/AppDesignUser.tsx
+++ src/components/AppDesignUser/AppDesignUser.tsx
@@ -9,7 +9,7 @@ export default function AppDesignUser(){
             </div>
             <div className={style.block}>
                 <div className={style.round2}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor"
                         className="bi bi-calendar2-week" viewBox="0 0 16 16">
                         <path
                             d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
