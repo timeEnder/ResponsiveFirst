b2350b8ddc7ea04a3ca78c17c94478381fe53d70
timeEnder <sh.s.ganiev@gmail.com>
2023-06-09 11:32:20 +0500

    chan

--- src/pages/AppDesign/AppDesign.tsx
+++ src/pages/AppDesign/AppDesign.tsx
@@ -8,6 +8,10 @@ export default function AppDesign(){
             <AppDesignHeader/>
             <div className={style.title}>App Design<div className={style.text}>Task manger ui kit</div></div>
             <AppDesignUser/>
+            <div className={style.description}>
+                <div className={style.desc_title}>Description</div>
+                <div className={style.desc_text}>Task manager app ul kit must be uniqe and trendy. In this app we need every features and section about task manager</div>
+            </div>
         </div>
     )
 }
